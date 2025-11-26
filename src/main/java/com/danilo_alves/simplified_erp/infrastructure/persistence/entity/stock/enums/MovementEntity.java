e9814bc74ae6bf0b342c896f28d33c5a7a32be9e
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-11-26 14:43:31 -0300

    feat: add enum movement entity

---
+++ src/main/java/com/danilo_alves/simplified_erp/infrastructure/persistence/entity/stock/enums/MovementEntity.java
@@ -0,0 +1,14 @@
+package com.danilo_alves.simplified_erp.infrastructure.persistence.entity.stock.enums;
+
+public enum MovementEntity {
+    SALE,
+    ENTRY,
+    EXIT,
+    RETURN,
+    ADJUSTMENT,
+    TRANSFER,
+    PURCHASE,
+    PRODUCTION,
+    CONSUMPTION,
+    OTHER
+}
